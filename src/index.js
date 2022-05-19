0e30383cecba1b49d994f6ed94a6d5a232ae8782
Akshay-Tarde <akshaymtarde@gmail.com>
2022-05-19 09:08:07 +0530

    Added a nav-bar with minimal styling.

--- src/index.js
+++ src/index.js
@@ -5,23 +5,47 @@ import './index.css';
 function ReactFacts() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src='./logo512.png' width="20px"/>
-        </nav>
-      </header>
-      <h1>Reasons I am excited to learn React.</h1>
+      <Header/>
+     <List/>
+     <Footer/>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+    <nav className='nav'>
+      <img className='logo' src='/react-logo.png' />
+      <ul className='nav-items'>
+        <li>Pricing</li> 
+        <li>About</li>
+        <li>Contact</li>  
+      </ul>
+    </nav>
+  </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+    <small> © 2022 Akshay Tarde development. All rights reserved. </small>
+    </footer>
+  )
+}
+
+function List() {
+  return (
+    <div>
+       <h1>Reasons I am excited to learn React.</h1>
       <ol>
       <li>Declarative</li>
       <li>Component Based</li>
       <li>Lots of Money involved.</li>
       </ol>
-      <footer>
-      © 2022 Tarde development. All rights reserved.
-      </footer>
     </div>
   )
 }
-  
 
 ReactDOM.render(<ReactFacts/>, document.getElementById("root"))
